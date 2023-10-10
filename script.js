914c7e552200775fc09f721ca58977ce99136cfe
LearnerVas <sorrowamv@gmail.com>
2023-10-10 20:25:17 +0800

    Fourth Activity in Data Structures

---
+++ script.js
@@ -0,0 +1,13 @@
+let fruit = "banana";
+if(fruit.toLowerCase("banana")){
+    console.log("Banana is good");
+}
+else if(fruit.toLowerCase("Orange")){
+    console.log("I am not a fan of oranges");
+}
+else if(fruit.toLowerCase("Apple")){
+    console.log("How do you like them apples?");
+}
+else {
+    console.log("I have never heard that fruit");
+}
